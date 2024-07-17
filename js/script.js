adb70aae44e62f9e4170d1b0b70839d346eb3b75
btrindadefm <btrindade172@gmail>
2024-07-17 03:10:25 -0300

    feat: adicionando nova funcionalidade de restartar o game com um clique em qualquer tecla do teclado

--- js/script.js
+++ js/script.js
@@ -2,21 +2,41 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const score = document.querySelector('.score');
 let scorePlacar = 0;
+let gameRunning = true; 
 
 const jump = () => {
-    mario.classList.add('jump');
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500);
+    if (gameRunning) {
+        mario.classList.add('jump');
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
 }
 
-const loop = setInterval(() => {
+const startGame = () => {
+    gameRunning = true;
+    scorePlacar = 0;
+    score.textContent = 'SCORE ' + scorePlacar;
 
+    pipe.style.animation = '';
+    pipe.style.left = '';
+
+    mario.style.animation = '';
+    mario.style.bottom = '';
+
+    mario.src = './img/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0px';
+
+    document.addEventListener('keydown', jump);
+    loop = setInterval(gameLoop, 10);
+}
+
+const gameLoop = () => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = + window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
@@ -24,21 +44,31 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './img/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '40px'
+        mario.style.width = '75px';
+        mario.style.marginLeft = '40px';
 
         clearInterval(loop);
+        gameRunning = false; 
+
+        document.removeEventListener('keydown', jump);
+        document.addEventListener('keydown', restartGame);
     } else {
         scoreGrowth();
     }
+}
 
-}, 10);
+const restartGame = () => {
+    document.removeEventListener('keydown', restartGame);
+    startGame();
+}
 
 document.addEventListener('keydown', jump);
 
 const scoreGrowth = () => {
-    scorePlacar = scorePlacar + 1;
-    score.textContent = 'SCORE  ' + scorePlacar;
+    if (gameRunning) {
+        scorePlacar = scorePlacar + 1;
+        score.textContent = 'SCORE ' + scorePlacar;
+    }
 }
 
-
+let loop = setInterval(gameLoop, 10);
